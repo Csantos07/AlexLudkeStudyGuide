2a7f1b7ee77c34494cdd3cf0338ba8c62d093f7a
Carlo Santos <rcarlosantos89@gmail.com>
2024-02-08 10:50:23 -0600

    working with operators

---
+++ script.js
@@ -0,0 +1,14 @@
+// Practicing with javascript
+
+// Operators
+// %, ++, --, **
+
+// let number = 3;
+// number = number++;
+// console.log(number);
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+console.log((101 % 2) === 0) // this checks to see if something is even
